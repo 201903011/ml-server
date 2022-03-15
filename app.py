b68f3bead14ede4ef65218d81fe323a5ef063480
Vinod Vaman Bhat <51293918+VVB2@users.noreply.github.com>
2022-03-15 20:14:30 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-#------------------------------------------------------------
-# Imports
-import time
 from flask import *
-
-from datetime import datetime
 from transformers import pipeline
 
 summarizer = pipeline("summarization")
 
-
 app = Flask(__name__)
-
 
 @app.route('/' , methods = ['POST'])
 def index():
@@ -18,8 +11,6 @@ def index():
     x = summarizer(arti, max_length=130, min_length=30, do_sample=False)
     print(x,"this is")
     return x[0]
-
-
 
 def meth(article):
     return summarizer(ARTICLE, max_length=130, min_length=30, do_sample=False)
